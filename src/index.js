11aa50c84bde7a0c46f111a2cd00ddc9658ed03b
gaudreaujacksoncharles <gaudreaujacksoncharles@gmail.com>
2016-11-18 05:31:34 +0000

    home basic

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
+/* dependencies */
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Router, Route, browserHistory, IndexRoute} from 'react-router';
 import App from './App';
+import './assets/css/reset';
 import './index.css';
 
+/* components */
+import Home from './pages/Home/Home';
+
 ReactDOM.render(
-  <App />,
+  <Router history={browserHistory}>
+    <Route path='/' component={App}>
+      <IndexRoute component={Home} />
+    </Route>
+  </Router>,
   document.getElementById('root')
 );
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,14 @@
+import React, {createClass} from 'react';
+import './Home.css';
+
+const Home = createClass({
+    render() {
+        return (
+            <main id='home'>
+                Home
+            </main>
+        );
+    }
+});
+
+export default Home;
