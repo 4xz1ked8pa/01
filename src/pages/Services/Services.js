9702d4dace1229a24153b830d3c1ae815967b738
gaudreaujacksoncharles <gaudreaujacksoncharles@gmail.com>
2016-11-30 17:57:39 +0000

    styles

--- src/pages/Services/Services.js
+++ src/pages/Services/Services.js
@@ -19,7 +19,26 @@ const Services = createClass({
                 <Navbar />
                 <div className='section'>
                     <Banner banner={{image: 'https://images.unsplash.com/photo-1438118907704-7718ee9a191a?dpr=2&auto=format&fit=crop&w=767&h=511&q=80&cs=tinysrgb&crop=', title: 'BECOME A PRODUCER', description: 'Nous simplifions la mise en marché des...'}} />
-                    
+                    <Tabs onSelect={this.handleSelect} selectedIndex={0} className='site-side-tabs'>
+                        <TabList>
+                        {
+                            services.map((service, index) => {
+                                return (
+                                    <Tab key={index}>{service.title}</Tab>
+                                )
+                            })
+                        }
+                        </TabList>
+                        {
+                            services.map((service, index) => {
+                                return (
+                                    <TabPanel key={index}>
+                                        {index}
+                                    </TabPanel>
+                                )
+                            })
+                        }
+                    </Tabs>
                 </div>
                 <Footer />
             </main>
