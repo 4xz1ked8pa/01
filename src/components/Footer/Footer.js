84ee765478805b42571b6348db8f18354936b286
gaudreaujacksoncharles <gaudreaujacksoncharles@gmail.com>
2016-11-30 17:50:54 +0000

    styles

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -18,8 +18,8 @@ const Footer = createClass({
                         <Link to={`/fr/`} className='page-link'>Français</Link>
                     </div>
                     <div className='social-links'>
-                        <a to={`https://www.facebook.com/microhabitatmtl/`} className='social-link fa fa-facebook'></a>
-                        <a to={`https://www.instagram.com/microhabitatmtl/`} className='social-link fa fa-instagram'></a>
+                        <a href='https://www.facebook.com/microhabitatmtl/' className='social-link fa fa-facebook'></a>
+                        <a href='https://www.instagram.com/microhabitatmtl/' className='social-link fa fa-instagram'></a>
                     </div>
                 </div>
             </div>
